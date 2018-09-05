b81518fb03fe6b3c2b4bd34061139477cd7f7258
Pavel Vasianovych <pppp_84@ukr.net>
2018-09-06 01:20:50 +0300

    JenTest1

---
+++ JenTest.java
@@ -0,0 +1,11 @@
+public class JenTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(("jenkins!!!"));
+    }
+    
+}
